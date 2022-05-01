55a84179dcf4a8200fc0ff0a0f7c366e9ec19769
R. Fariz <farizramazanov38@gmail.com>
2022-05-01 21:49:39 +0400

    docs: throw error on vigenere task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(isDirect = true){
+    this.isDirect = isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if(message === undefined || key === undefined){
+      throw new Error("Incorrect arguments!")
+    }
+    let text = "";
+
+
+    return text;
+  }
+  decrypt(encryptedMessage, key) {
+    if(encryptedMessage === undefined || key === undefined){
+      throw new Error("Incorrect arguments!")
+    }
+    let text = "";
+
+
+    return text;
   }
 }
 
